162ead6a793bfed01bc01ef67d62ce4b5489a559
BtcDrak <btcdrak@gmail.com>
2016-02-16 22:23:28 +0000

    RPC tests for BIP112 soft fork

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -111,6 +111,7 @@ testScriptsExt = [
     'bip65-cltv.py',
     'bip65-cltv-p2p.py',
     'bip68-sequence.py',
+    'bip112-csv-p2p.py',
     'bipdersig-p2p.py',
     'bipdersig.py',
     'getblocktemplate_longpoll.py',
